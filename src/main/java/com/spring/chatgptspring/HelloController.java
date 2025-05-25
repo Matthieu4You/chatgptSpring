9b8331bbc29532a8adb8fbfec64cfd82a3549132
Mattu Zhou <zhicongzhoumattu2021@gmail.com>
2025-05-25 20:01:36 +0800

    change road name

--- src/main/java/com/spring/chatgptspring/HelloController.java
+++ src/main/java/com/spring/chatgptspring/HelloController.java
@@ -2,6 +2,7 @@ package com.spring.chatgptspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,8 +15,8 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
-    @GetMapping("/hello/${name}")
-    public String hello(String name){
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name){
         return helloService.sayHello(name);
     }
 
